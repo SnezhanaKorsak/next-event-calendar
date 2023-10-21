d4569c4fe160a1f610b45fb7de03318473288bde
SnezhanaKorsak <snezhana_korsak@mail.ru>
2023-10-21 14:17:04 +0300

    Add Page for selected event

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,7 +1,27 @@
-export default function EventDetailPage () {
+import { Fragment } from "react";
+import { useRouter } from "next/router"
+import { getEventById } from "@/dynamicData";
+import EventSummary from "@/components/eventDetail/event-summary";
+import EventLogistics from "@/components/eventDetail/event-logistics";
+import EventContent from "@/components/eventDetail/event-content";
+
+export default function EventDetailPage() {
+  const router = useRouter();
+
+  const { eventId } = router.query
+  const event = getEventById(eventId)
+
+  if (!event) {
+    return <p>No event found</p>
+  }
+
   return (
-    <div>
-      <h1>Event Detail Page</h1>
-    </div>
+    <Fragment>
+      <EventSummary title={event.title}/>
+      <EventLogistics date={event.date} address={event.location} image={event.image} imageAlt={event.title}/>
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </Fragment>
   );
 }
